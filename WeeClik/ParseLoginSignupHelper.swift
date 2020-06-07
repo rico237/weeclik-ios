1b10d4d2cc3667efa10a453a869fa09a8d8d390f
Rico237 <wolbereric@gmail.com>
2020-06-07 19:58:29 +0200

    Clean up old comments

--- WeeClik/ParseLoginSignupHelper.swift
+++ WeeClik/ParseLoginSignupHelper.swift
@@ -11,25 +11,26 @@ import Parse
 class ParseLoginSignupHelper {
     static func parseLoginViewController() -> PFLogInViewController {
         let logInController = LoginViewController()
-            logInController.fields = [PFLogInFields.usernameAndPassword,
-                                      PFLogInFields.logInButton,
-                                      PFLogInFields.signUpButton,
-                                      PFLogInFields.passwordForgotten,
-                                      PFLogInFields.dismissButton,
-                                      PFLogInFields.facebook]
-            logInController.emailAsUsername = true
-            logInController.facebookPermissions = ["email", "public_profile"]
+        logInController.fields = [.usernameAndPassword,
+                                  .logInButton,
+                                  .signUpButton,
+                                  .passwordForgotten,
+                                  .dismissButton,
+                                  .facebook]
+        logInController.emailAsUsername = true
+        logInController.facebookPermissions = ["email", "public_profile"]
 
-            // SignUp Part
-            logInController.signUpController = SignUpViewController()
-    //        logInController.signUpController?.emailAsUsername = true
-            logInController.signUpController?.fields = [.usernameAndPassword, .signUpButton, .additional, .dismissButton]
-            logInController.signUpController?.signUpView?.usernameField?.keyboardType = .emailAddress
-            logInController.signUpController?.signUpView?.additionalField?.isSecureTextEntry = true
-            logInController.signUpController?.signUpView?.additionalField?.keyboardType = .alphabet
-    //        logInController.signUpController?.signUpView?.additionalField?.textContentType = .password
-            logInController.signUpController?.signUpView?.usernameField?.placeholder = "Email".localized()
-            logInController.signUpController?.signUpView?.additionalField?.placeholder = "Confirmation du mot de passe".localized()
+        // SignUp Part
+        logInController.signUpController = SignUpViewController()
+        logInController.signUpController?.fields = [.usernameAndPassword,
+                                                    .signUpButton,
+                                                    .additional,
+                                                    .dismissButton]
+        logInController.signUpController?.signUpView?.usernameField?.keyboardType = .emailAddress
+        logInController.signUpController?.signUpView?.additionalField?.isSecureTextEntry = true
+        logInController.signUpController?.signUpView?.additionalField?.keyboardType = .alphabet
+        logInController.signUpController?.signUpView?.usernameField?.placeholder = "Email".localized()
+        logInController.signUpController?.signUpView?.additionalField?.placeholder = "Confirmation du mot de passe".localized()
         return logInController
     }
 }
